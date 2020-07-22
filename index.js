ae95281b1ba92c13a40d9fd173eaf314f992ca84
mkqcc <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-07-22 15:50:30 -0400

    Finish whack-a-mole

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-
-
 let score = 0;
 var scoreDisplay = document.getElementById('score');
 
-console.log(scoreDisplay);
+let holes = document.getElementsByClassName("hole")
+
+let randomHoleIndex = Math.floor(Math.random() * holes.length)
+setInterval(() => {holes[randomHoleIndex].classList.toggle("mole")}, 1500)
+
+const gameArea = document.getElementById('whack-a-mole');
+gameArea.addEventListener('click', event => {
+    if (event.target.matches('.mole')) {
+        ++score
+        holes[randomHoleIndex].classList.remove("mole")
+        randomHoleIndex = Math.floor(Math.random() * holes.length)
+    }
+    scoreDisplay.innerHTML = score
+})
